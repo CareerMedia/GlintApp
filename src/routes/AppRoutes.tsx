75c54b5ff4090e00716c253bd1d4bb3ae689571d
CareerMedia <careermedia@csun.edu>
2025-10-25 18:02:33 -0700

    Update AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,27 +1,22 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from '../pages/Home';
-import Map from '../pages/Map';
-import Send from '../pages/Send';
-import Transfer from '../pages/Transfer';
-import Success from '../pages/Success';
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from '../pages/Home'
+import Map from '../pages/Map'
+import Send from '../pages/Send'
+import Transfer from '../pages/Transfer'
+import Success from '../pages/Success'
 
-export default function AppRoutes() {
+function AppRoutes() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/map" element={<Guard><Map /></Guard>} />
-        <Route path="/send" element={<Guard><Send /></Guard>} />
-        <Route path="/transfer" element={<Guard><Transfer /></Guard>} />
-        <Route path="/success" element={<Guard><Success /></Guard>} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </HashRouter>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/map" element={<Map />} />
+      <Route path="/send/:country" element={<Send />} />
+      <Route path="/transfer" element={<Transfer />} />
+      <Route path="/success" element={<Success />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
 }
 
-function Guard({ children }: { children: JSX.Element }) {
-  const signedIn = typeof window !== 'undefined' && localStorage.getItem('signedIn') === 'true';
-  if (!signedIn) return <Navigate to="/" replace />;
-  return children;
-}
+export default AppRoutes
+export { AppRoutes }
